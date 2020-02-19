363dc54f669ac78c4cc967524992a259632c403b
David Lau <fatesky2@163.com>
2020-02-19 16:18:40 +0800

    changed to for

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World!");
-		System.out.println("Nice to meet you!");
+		for (int i = 0; i < 5; i++){
+			System.out.println("Nice to meet you!");
+		}
 	}
 }
